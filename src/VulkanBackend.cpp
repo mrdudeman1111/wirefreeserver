03ac86883179a613cc151faab8f5d28a170c5c3c
Ethan <gaemsgy@gmail.com>
2023-01-18 10:39:10 -0600

    Writing vulkan backend

--- src/VulkanBackend.cpp
+++ src/VulkanBackend.cpp
@@ -9,4 +9,21 @@ void Init()
 {
     VkInstanceCreateInfo InstanceCI{};
     InstanceCI.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    InstanceCI.layerCount = InstanceLayers.size();
+    InstanceCI.ppLayerNames = InstanceLayers.data();
+
+    if(VkCreateInstance(&InstanceCI, nullptr, &Instance) != VK_SUCCESS)
+    {
+        throw std::runtime_error("Failed to create Instance");
+    }
+
+    uint32_t PDevCount;
+    vkEnumeratePhysicalDevices(Instance, &PDevCount, nullptr);
+    std::vector<VkPhysicalDevice> PDevices(PDevCount);
+    vkEnumeratePhysicalDevices(Instance, &PDevCount, PDev.data());
+
+    for(uint32_t i = 0; i < PDevCount; i++)
+    {
+        
+    }
 }
