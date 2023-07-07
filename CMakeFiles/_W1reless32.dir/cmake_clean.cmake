d8a63c79e8fa318a11c18ec40f77ac938ab09f11
mrdudeman1111 <gaemsgy@gmail.com>
2023-07-07 14:00:16 -0500

    the Compositor is failing to create shared vulkan textures on linux for some reason.

---
+++ CMakeFiles/_W1reless32.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/ethanw/.steam/steam/steamapps/common/SteamVR/drivers/W1reless/bin/linux32/driver_W1reless.pdb"
+  "/home/ethanw/.steam/steam/steamapps/common/SteamVR/drivers/W1reless/bin/linux32/driver_W1reless.so"
+  "CMakeFiles/_W1reless32.dir/linux64/Display.cpp.o"
+  "CMakeFiles/_W1reless32.dir/linux64/Display.cpp.o.d"
+  "CMakeFiles/_W1reless32.dir/linux64/Headset.cpp.o"
+  "CMakeFiles/_W1reless32.dir/linux64/Headset.cpp.o.d"
+  "CMakeFiles/_W1reless32.dir/linux64/VulkanBackend.cpp.o"
+  "CMakeFiles/_W1reless32.dir/linux64/VulkanBackend.cpp.o.d"
+  "CMakeFiles/_W1reless32.dir/linux64/driver.cpp.o"
+  "CMakeFiles/_W1reless32.dir/linux64/driver.cpp.o.d"
+  "CMakeFiles/_W1reless32.dir/linux64/vrDriver.cpp.o"
+  "CMakeFiles/_W1reless32.dir/linux64/vrDriver.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_W1reless32.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/_W1reless.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/run/media/ethanw/LinuxGames/Repos/wirefreeserver/linux64/Display.cpp" "CMakeFiles/_W1reless.dir/linux64/Display.cpp.o" "gcc" "CMakeFiles/_W1reless.dir/linux64/Display.cpp.o.d"
+  "/run/media/ethanw/LinuxGames/Repos/wirefreeserver/linux64/Headset.cpp" "CMakeFiles/_W1reless.dir/linux64/Headset.cpp.o" "gcc" "CMakeFiles/_W1reless.dir/linux64/Headset.cpp.o.d"
+  "/run/media/ethanw/LinuxGames/Repos/wirefreeserver/linux64/VulkanBackend.cpp" "CMakeFiles/_W1reless.dir/linux64/VulkanBackend.cpp.o" "gcc" "CMakeFiles/_W1reless.dir/linux64/VulkanBackend.cpp.o.d"
+  "/run/media/ethanw/LinuxGames/Repos/wirefreeserver/linux64/driver.cpp" "CMakeFiles/_W1reless.dir/linux64/driver.cpp.o" "gcc" "CMakeFiles/_W1reless.dir/linux64/driver.cpp.o.d"
+  "/run/media/ethanw/LinuxGames/Repos/wirefreeserver/linux64/vrDriver.cpp" "CMakeFiles/_W1reless.dir/linux64/vrDriver.cpp.o" "gcc" "CMakeFiles/_W1reless.dir/linux64/vrDriver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
